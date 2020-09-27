96c5532cee394ab649caa56cb63bd78c8674f695
piyo02 <alzidni2000@gmail.com>
2020-09-27 17:37:58 +0800

    push

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,15 @@
 {
-    'name': 'Template Addon',
+    'name': 'History Price SO',
     'author': 'technoindo.com',
     'category': 'hidden',
     'version': '10.0',
     'summary': 'Summary the addon.',
     'description': '''Description the addon'''
                    ,
-    'depends': ['base'],
+    'depends': ['sale'],
     'data': [
-        ''
+        'views/history_price.xml',
+        'views/sale_order.xml'
     ],
     'images': [''],
     'auto_install': False,
